ee275076774b1cfa15dc3653ce0da54661728c5d
Maryam Estarda Bañuelos <maestradaba@ittepic.edu.mx>
2026-02-15 22:42:44 -0700

    refactor: mueve Punto al paquete correcto

--- Proyecto1/src/proyecto1/Distancia.java
+++ Proyecto1/src/proyecto1/Distancia.java
@@ -2,11 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+package proyecto1;
 
 /**
  *
  * @author ESTRADA
  */
-public class Punto {
+public class Distancia {
     
 }
